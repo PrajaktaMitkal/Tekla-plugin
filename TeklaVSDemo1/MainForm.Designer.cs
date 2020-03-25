d6abf5911cbef497f3faa46416681fee688ba50e
PrajaktaMitkal <39733191+PrajaktaMitkal@users.noreply.github.com>
2020-03-25 20:20:57 +0530

    Add files via upload

---
+++ TeklaVSDemo1/MainForm.Designer.cs
@@ -0,0 +1,113 @@
+﻿using System.Windows.Forms;
+
+namespace TeklaVSDemo1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.structuresExtender.SetAttributeName(this.label1, null);
+            this.structuresExtender.SetAttributeTypeName(this.label1, null);
+            this.label1.AutoSize = true;
+            this.structuresExtender.SetBindPropertyName(this.label1, null);
+            this.label1.Location = new System.Drawing.Point(23, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Height";
+            // 
+            // textBox1
+            // 
+            this.structuresExtender.SetAttributeName(this.textBox1, null);
+            this.structuresExtender.SetAttributeTypeName(this.textBox1, null);
+            this.structuresExtender.SetBindPropertyName(this.textBox1, null);
+            this.textBox1.Location = new System.Drawing.Point(67, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.structuresExtender.SetAttributeName(this.button1, null);
+            this.structuresExtender.SetAttributeTypeName(this.button1, null);
+            this.structuresExtender.SetBindPropertyName(this.button1, null);
+            this.button1.Location = new System.Drawing.Point(26, 138);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.structuresExtender.SetAttributeName(this.button2, null);
+            this.structuresExtender.SetAttributeTypeName(this.button2, null);
+            this.structuresExtender.SetBindPropertyName(this.button2, null);
+            this.button2.Location = new System.Drawing.Point(107, 138);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "MODIFY";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // MainForm
+            // 
+            this.structuresExtender.SetAttributeName(this, null);
+            this.structuresExtender.SetAttributeTypeName(this, null);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.structuresExtender.SetBindPropertyName(this, null);
+            this.ClientSize = new System.Drawing.Size(317, 173);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "MainForm";
+            this.Text = "User plugin form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Button button1;
+        private Button button2;
+    }
+}
---
+++ TeklaVSDemo1/MainPlugin.cs
@@ -0,0 +1,72 @@
+﻿using Tekla.Structures.Drawing;
+using Tekla.Structures.Datatype;
+using Tekla.Structures.Plugins;
+using System.Collections.Generic;
+using Tekla.Structures.Geometry3d;
+using System.Windows.Forms;
+using System;
+using Tekla.Structures.Model;
+using Tekla.Structures.Model.UI;
+
+namespace TeklaVSDemo1
+{
+    [Tekla.Structures.Plugins.Plugin("TeklaVSDemo1")]
+    [Tekla.Structures.Plugins.PluginUserInterface("TeklaVSDemo1.MainForm")]
+    public class MainPlugin : PluginBase
+    {
+        private readonly Model _model;
+        public Model model1 { get { return _model; } }
+        
+        private StructuresData data { get; set; }
+
+        public MainPlugin(StructuresData structData)
+        {
+            data = structData;
+            _model = new Model();
+         }
+        public override List<InputDefinition> DefineInput()
+        {
+            Picker pointPicker = new Picker();
+            List<InputDefinition> PointList = new List<InputDefinition>();
+            Point inputPoints = pointPicker.PickPoint();
+            InputDefinition inputDef = new InputDefinition(inputPoints);
+            PointList.Add(inputDef);
+            return PointList;
+
+            //throw new System.NotImplementedException();
+        }
+
+        public override bool Run(List<InputDefinition> input)
+        {
+            try
+            {
+                double _height = data.height;
+                // Get picked point in Tekla Structures
+                Point startpoint = (Point)input[0].GetInput();
+                // Calculate the end point of the beam
+                Point endPoint = new Point(startpoint);
+                endPoint.Z += _height;
+                // Create a beam instance
+                Beam column = new Beam(startpoint, endPoint);
+                column.Profile.ProfileString = "HEA400";
+
+                //Insert beam in model
+                column.Insert();
+
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+            return true;
+        }
+        public void getDetailsBeam()
+        {
+            Beam beam = new Beam();
+            MessageBox.Show(beam.Name);
+           
+                
+        }
+    }
+}
